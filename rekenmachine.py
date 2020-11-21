c0c6c0208c62f55aafab39b0b2aa5aae6100f089
mithrandil444 <sven.devisscher@student.kdg.be>
2020-11-21 15:31:24 +0100

    versie 1.4

--- rekenmachine.py
+++ rekenmachine.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 """
 Met dit rekenmachine kan je optellen,aftrekken,vermenigvuldigen en delen door 2 getallen te kiezen
-bron : https://www.digitalocean.com/community/tutorials/how-to-make-a-calculator-program-in-python-3
+bron 1: https://www.digitalocean.com/community/tutorials/how-to-make-a-calculator-program-in-python-3
+bron 2 : https://www.programiz.com/python-programming/examples/calculator
 """
 
 
@@ -11,31 +12,31 @@ __status__ = "Development"
 
 
 # Variabelen die je definieert
-Number_1 = int(input("Pick a number: "))       # Input, pick your first number
-Number_2 = int(input("Pick another number: ")) # Input, it lets you pick a second number4
+Getal_1 = int(input("Pick a number: "))        # Input, kies je eerste getal
+Getal_2 = int(input("Pick another number: "))  # Input, kies je 2de getal
 
-what = input('''What would you like to do?
-+ for addition
-- for subtraction
-* for multiplication
-/ for division
+what = input('''What would you like to do?     # wat wil je doen?
+optellen for addition
+aftrekken for subtraction
+vermenigvuldigen for multiplication
+delen for division
 ''')
 
+
 def main():
-    if what == "+":
-        print(Number_1 + Number_2)
+    if what == "optellen":
+        print(Getal_1 + Getal_2)
 
-    elif what == '-':
-        print(Number_1 - Number_2)
+    elif what == 'aftrekken':
+        print(Getal_1 - Getal_2)
 
-    elif what == '*':
-        print(Number_1 * Number_2)
+    elif what == 'vermenigvuldigen':
+        print(Getal_1 * Getal_2)
 
-    elif what == '*':
-        print(Number_1 / Number_2)
+    elif what == 'delen':
+        print(Getal_1 / Getal_2)
 
-    else:
-        print('this is not a valid number')
+
 
 
 if __name__ == '__main__':  # code to execute if called from command-line
