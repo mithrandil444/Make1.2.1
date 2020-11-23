6a2e5543360884536e9be78bdf8fb349e20d204e
mithrandil444 <sven.devisscher@student.kdg.be>
2020-11-23 11:01:23 +0100

    versie 1.3

--- systeemautomatisatie.py
+++ systeemautomatisatie.py
@@ -1,23 +1,6 @@
-#!/usr/bin/env python
-"""
-Dit is een scriptje dat automatisch updates doet op je RPI
-"""
-
-# IMPORTS
-
-
-__author__ = "Sven De Visscher "
-__email__ = "sven.devisscher@student.kdg.be"
-__status__ = "Development"
-
-
-# CONFIGURING I/O
-
-
-def main():
+#!/bin/bash
 
 sudo apt update && apt upgrade -y
 
 
-if __name__ == '__main__':  # code to execute if called from command-line
-    main()
+
